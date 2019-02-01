9429d536f26a95bd9ad49516ae111df418abf6c6
Dávid Mátyás <davimatyi@outlook.com>
2019-02-01 11:48:38 +0100

    vírus textúra + farok atlas

--- core/src/hu/csanysoft/donto/Actors/BadVirus.java
+++ core/src/hu/csanysoft/donto/Actors/BadVirus.java
@@ -4,13 +4,19 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 
 import hu.csanysoft.donto.Global.Assets;
 import hu.csanysoft.donto.Global.Globals;
+import hu.csanysoft.donto.MyBaseClasses.Scene2D.OffsetSprite;
 
 public class BadVirus extends Virus {
 
     public boolean needsABetterWeaponToDestroy ;
 
+    OffsetSprite body;
+
     public BadVirus() {
-        super(Assets.manager.get(Assets.MOSQUITO_TEXTURE));
+        super();
         needsABetterWeaponToDestroy = Globals.random(0, 20) < 4;
+
+        body = new OffsetSprite(Assets.manager.get(Assets.BADVIRUS_TEXTURE), 0,0,64,64);
+        addSprite(body);
     }
 }

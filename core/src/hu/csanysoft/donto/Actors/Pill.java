663938b247cba95eade37250e153b7700bbb9305
Dávid Mátyás <davimatyi@outlook.com>
2019-02-01 13:21:28 +0100

    pirula elhelyezve 5 szintenként egyel több

--- core/src/hu/csanysoft/donto/Actors/Pill.java
+++ core/src/hu/csanysoft/donto/Actors/Pill.java
@@ -6,10 +6,11 @@ import hu.csanysoft.donto.Global.Assets;
 import hu.csanysoft.donto.MyBaseClasses.Scene2D.OneSpriteStaticActor;
 
 public class Pill extends OneSpriteStaticActor {
-    public Pill(float x, float y) {
+    public Pill(float x, float y, float rotation) {
         super(Assets.manager.get(Assets.EXIT));
         setSize(100, 50);
         setPosition(x,y);
+        setRotation(rotation);
         addBaseCollisionRectangleShape();
     }
 }
